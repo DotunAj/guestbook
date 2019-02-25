22c3cb774eef5a63a1ddcf7b6ec6f7d4776f1c32
Ayodotun <dotunajala@gmail.com>
2019-02-25 01:06:02 +0100

    Set up handlebar template engine with express

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// Handlebar Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set static assets
+app.use(express.static(path.join(__dirname, "public")));
+
+// Render Index page
+app.get("/", (req, res) => res.render("index"));
+
+// Render View page
+app.get("/view.html", (req, res) => res.render("view"));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server started on port", PORT));
