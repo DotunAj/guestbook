f1ddb8c1c4153ef7e324ed45ed798d71e8c2b330
Ayodotun <dotunajala@gmail.com>
2019-02-25 13:31:59 +0100

    Create dummy db

---
+++ db/guests.js
@@ -0,0 +1,25 @@
+const guests = [
+  {
+    id: 1,
+    name: "Abati Ace Adewale",
+    email: "acekyd01@gmail.com",
+    comment:
+      "Semi-Genius. Gamer. Full Stack Developer. Co-host @mintpodcast. Knowledge Seeker. Cavs 🏀. Marvel #MCU. Ace ♠ Developer."
+  },
+  {
+    id: 2,
+    name: "Ayodotun Ayodeji Ajala",
+    email: "dotman@gmail.com",
+    comment:
+      "Semi-Genius. Gamer. Full Stack Developer. Co-host @mintpodcast. Knowledge Seeker. Cavs 🏀. Marvel #MCU. Ace ♠ Developer."
+  },
+  {
+    id: 3,
+    name: "Makanjuola Olamide Faith",
+    email: "mkft@gmail.com",
+    comment:
+      "Semi-Genius. Gamer. Full Stack Developer. Co-host @mintpodcast. Knowledge Seeker. Cavs 🏀. Marvel #MCU. Ace ♠ Developer."
+  }
+];
+
+module.exports = guests;
